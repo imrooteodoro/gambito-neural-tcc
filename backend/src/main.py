77bcb21884a5be9f2b1784f312d1f14144c10b8a
imrooteodoro <adelsonteodoro67@gmail.com>
2025-11-07 21:28:20 -0300

    feat: muda mensagem do página main

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI, status
 
 app = FastAPI()
 
+
+
 @app.get("/health", status_code=status.HTTP_200_OK)
 async def health_route() -> dict:
     return {"Chess app is running"}
