22157728f5363e89642caea9c3094e7c564ab875
imrooteodoro <adelsonteodoro67@gmail.com>
2025-11-08 15:57:35 -0300

    feat: adiciona configs do langchain

---
+++ backend/src/core/langchain_config.py
@@ -0,0 +1,21 @@
+from langchain import chat_models
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+variables = {
+    "MODEL_NAME": os.getenv("MODEL_NAME"),
+    "MODEL_PROVIDER": os.getenv("MODEL_PROVIDER"),
+    "GENAI_API_KEY": os.getenv("GENAI_API_KEY"),
+}
+
+for variable in variables:
+    if variables[variable] is None:
+        raise ValueError(f"Environment variable {variable} is not set.")
+
+chess_agent = chat_models.init_chat_model(
+    model=variables["MODEL_NAME"],
+    model_provider= variables["MODEL_PROVIDER"],
+    api_key=variables["GENAI_API_KEY"],
+    )
