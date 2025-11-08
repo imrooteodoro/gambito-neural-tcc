098ae00aa9ddc757edf20300f805ee88108ee05c
imrooteodoro <adelsonteodoro67@gmail.com>
2025-11-08 15:56:40 -0300

    feat: adiciona rota para analize de partidas

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI, status
+from src.routes.genai_routes import router as genai_router
 
-app = FastAPI()
-
+app = FastAPI(
+    title="Chess Application API",
+    description="API based in GenAI to analyze chess positions.",
+    version="1.0.0",
+    contact={
+        "name": "Neural Gambit",
+        "email": 'imrooteodoro.ai.dev@gmail.com' }  
+)
 
 
 @app.get("/health", status_code=status.HTTP_200_OK)
 async def health_route() -> dict:
-    return {"Chess app is running"}
+    return {"status": "Chess app is running"}
+
+app.include_router(genai_router, prefix="/genai", tags=["GenAI"])
