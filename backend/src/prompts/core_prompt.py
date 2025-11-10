f414123f5bfc6dd78e3ffdb8036b913df32da65a
imrooteodoro <adelsonteodoro67@gmail.com>
2025-11-09 23:38:00 -0300

    feat: adiciona prompt para tradução de jogadas da chessengine para o LLM

--- backend/src/prompts/core_prompt.py
+++ backend/src/prompts/core_prompt.py
@@ -15,3 +15,22 @@ core_prompt = PromptTemplate(
     input_variables=["user_message"],
     template=CORE_PROMPT + "\nUser Message: {user_message}\n",
 )
+
+TRANSLATE_CHESSENGINE_OUTPUT_PROMPT = """
+
+You are an expert in chess and an AI language model named Neural Gambit AI. 
+Your task is to translate the output from a chess engine into Brazilian Portuguese, 
+ensuring that the translation is clear, accurate, and uses appropriate chess terminology.
+
+Respond in a concise and clear manner, focusing on the key aspects of the engine's output.
+
+The responses of engine like "score" and "pv" - "pv" means "principal variation" and not explain pieces name:
+excample: "e2e4" is about move e2 pawn to e4 - should be translated into Brazilian Portuguese with terminology and expressions from brazilian chess,
+like "DUPLO", "GARFO".
+
+"""
+
+translate_chessengine_output_prompt = PromptTemplate(
+    input_variables=["user_message", "moves"],
+    template=TRANSLATE_CHESSENGINE_OUTPUT_PROMPT + "\nUser Message: {user_message}\n",
+)      
