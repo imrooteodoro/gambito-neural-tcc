93c786f0137fe781f8345d996b7fcd659a3e4009
imrooteodoro <adelsonteodoro67@gmail.com>
2025-11-12 21:22:48 -0300

    feat: cria relacionamento 1:n para a tabelas

--- backend/src/models/User.py
+++ backend/src/models/User.py
@@ -1,5 +1,7 @@
-from sqlalchemy import Table, Column, Integer, String, Boolean
-from src.db.db_connection import *
+from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
+from sqlalchemy.orm import relationship
+from src.db.db_connection import Base, engine
+
 
 class Users(Base):
     __tablename__ = 'users'
@@ -11,29 +13,47 @@ class Users(Base):
     level = Column(Integer, default=1)
     is_active = Column(Boolean, default=True)
 
+    studies = relationship("Studies", back_populates="user", cascade="all, delete-orphan")
+    games = relationship("GameHistory", back_populates="user", cascade="all, delete-orphan")
+    preferences = relationship("Preferences", back_populates="user", cascade="all, delete-orphan")
+    statistics = relationship("Statistics", back_populates="user", cascade="all, delete-orphan")
+
+
 class Studies(Base):
     __tablename__ = 'studies'
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id', ondelete="CASCADE"))
     study_data = Column(String, nullable=False)
+
+    user = relationship("Users", back_populates="studies")
+
 
 class GameHistory(Base):
     __tablename__ = 'game_history'
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, nullable=False)
-    game_data = Column(String, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id', ondelete="CASCADE"))
+    game_pgn = Column(String, nullable=False)
+
+
+    user = relationship("Users", back_populates="games")
+
 
 class Preferences(Base):
     __tablename__ = 'preferences'
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id', ondelete="CASCADE"))
     preference_data = Column(String, nullable=False)
+
+    user = relationship("Users", back_populates="preferences")
+
 
 class Statistics(Base):
     __tablename__ = 'statistics'
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id', ondelete="CASCADE"))
     statistics_data = Column(String, nullable=False)
+
+    user = relationship("Users", back_populates="statistics")
 
 
 Base.metadata.create_all(bind=engine)
