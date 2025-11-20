eaa36e4daf624778a45ba0b570d56d8d307e3dce
imrooteodoro <adelsonteodoro67@gmail.com>
2025-11-20 11:24:09 -0300

    feat: Add GameHistory, Studies, and Statistics methods to UserRepository

--- backend/src/repositories/user_repository.py
+++ backend/src/repositories/user_repository.py
@@ -1,4 +1,4 @@
-from src.models.User import Users
+from src.models.User import Users, GameHistory, Studies, Statistics
 from sqlalchemy.orm import Session
 import datetime
 
@@ -47,5 +47,44 @@ class UserRepository:
         if not user:
             return False
         self.db.delete(user)
+        self.db.commit()
+        return True
+
+    # GameHistory methods
+    def insert_game_history(self, game_history: GameHistory):
+        self.db.add(game_history)
+        self.db.commit()
+        self.db.refresh(game_history)
+        return game_history
+
+    def get_game_history_by_user(self, user_id: int):
+        return self.db.query(GameHistory).filter(GameHistory.user_id == user_id).all()
+
+    # Studies methods
+    def insert_study(self, study: Studies):
+        self.db.add(study)
+        self.db.commit()
+        self.db.refresh(study)
+        return study
+
+    def get_studies_by_user(self, user_id: int):
+        return self.db.query(Studies).filter(Studies.user_id == user_id).all()
+    
+    # Statistics methods
+    def insert_statistics(self, statistics: Statistics):
+        self.db.add(statistics)
+        self.db.commit()
+        self.db.refresh(statistics)
+        return statistics
+
+    def get_statistics_by_user(self, user_id: int):
+        return self.db.query(Statistics).filter(Statistics.user_id == user_id).all()
+
+    def delete_study(self, study_id: int) -> bool:
+        study = self.db.query(Studies).filter(Studies.id == study_id).first()
+        if not study:
+            return False
+
+        self.db.delete(study)
         self.db.commit()
         return True
