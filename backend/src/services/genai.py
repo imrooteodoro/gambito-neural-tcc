9c0da962f1d0bc5ea070b3870a8d04ec81dcd094
imrooteodoro <adelsonteodoro67@gmail.com>
2025-11-23 23:43:30 -0300

    fix: Correct parameter name in _run_translate_agent method for clarity

--- backend/src/services/genai.py
+++ backend/src/services/genai.py
@@ -9,8 +9,8 @@ class Genai:
         prompt = core_prompt.format(user_message=user_message)
         response = chess_agent.stream(prompt)
         return response
-    def _run_translate_agent(self, moves:list[str], user_message: str) -> str:
-        prompt = translate_chessengine_output_prompt.format(user_message=user_message, moves=moves)
+    def _run_translate_agent(self, moves:list[str], board_pgn: str) -> str:
+        prompt = translate_chessengine_output_prompt.format(moves=moves, board_pgn=board_pgn)
         response = chess_agent.stream(prompt)
         print("Translate Agent Response:", response)
         return response
